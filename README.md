b60ff49a5f2cd6ad0b442a2f172bd8b54b90190a
chukwumaokere <chukwumaokere@yahoo.com>
2024-06-19 00:16:27 -0500

    Add floor with no gravity but physics

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # React + TypeScript + Vite + React-Three-Fiber + Drei + Three.js
-https://github.com/pmndrs/drei
-https://docs.pmnd.rs/react-three-fiber/getting-started/introduction
-https://www.youtube.com/watch?v=DPl34H2ISsk
+https://github.com/pmndrs/drei  
+https://docs.pmnd.rs/react-three-fiber/getting-started/introduction  
+https://www.youtube.com/watch?v=DPl34H2ISsk  
+https://github.com/pmndrs/react-postprocessing  
+https://github.com/pmndrs/react-three-rapier  
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import * as THREE from 'three'
 import { Canvas, ThreeElements, useFrame } from '@react-three/fiber'
 import { OrbitControls, PerspectiveCamera } from '@react-three/drei'
-import { useRef, useState } from 'react';
+import { Suspense, useRef, useState } from 'react';
+import { Bloom, DepthOfField, EffectComposer, Noise, Vignette } from '@react-three/postprocessing'
+import { Physics, RigidBody } from "@react-three/rapier";
 
-function Box(props: ThreeElements['mesh']) {
+function Box(props: ThreeElements['mesh'] & {color?: string}) {
   const [active, setActive] = useState(false);
   const [hovered, setHovered] = useState(false);
   const meshRef = useRef<THREE.Mesh>(null!)
   
-  useFrame((_state, delta) => (meshRef.current.rotation.x += delta))
-  useFrame((_state, delta) => (meshRef.current.rotation.y += delta))
+  /* useFrame((_state, delta) => (meshRef.current.rotation.x += delta))
+  useFrame((_state, delta) => (meshRef.current.rotation.y += delta)) */
 
   return (
+    <RigidBody colliders={'hull'} restitution={0.01}>
       <mesh
         {...props}
         ref={meshRef}
@@ -21,8 +24,20 @@ function Box(props: ThreeElements['mesh']) {
         onPointerOut={() => setHovered(false)}
       >
         <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+        <meshStandardMaterial color={hovered ? 'hotpink' : props.color || 'orange'} />
       </mesh>
+    </RigidBody>
+  )
+}
+
+function Floor() {
+  return (
+    <RigidBody colliders={'hull'} gravityScale={0} restitution={1}>
+      <mesh position={[0, 0, -50]}>
+        <boxGeometry args={[100, 1, 100]} />
+        <meshStandardMaterial color="green" />
+      </mesh>
+    </RigidBody>
   )
 }
 
@@ -30,14 +45,26 @@ function App() {
   return (
     <Canvas>
       <ambientLight intensity={Math.PI / 2} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-      <PerspectiveCamera makeDefault fov={75} position={[0, 0, 5]} />
+      <directionalLight color="white" intensity={Math.PI} position={[0, 0, 5]} />
+      {/* <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} /> */}
+      <PerspectiveCamera makeDefault fov={75} position={[0, 20, 50]} />
       <OrbitControls makeDefault />
-      <group position={[0, 0, 0]}>
-        <Box position={[-1, 0, 0]} />
-        <Box position={[1, 0, 0]} />
-      </group>
+      <Suspense>
+        <Physics debug>
+          <group position={[0, 50, -50]}>
+            <Box position={[-1, 0, 0]} color='green' />
+            <Box position={[1, 0, 0]} />
+          </group>
+          <Floor />
+        </Physics>
+      </Suspense>
+      <EffectComposer>
+        <DepthOfField focusDistance={0} focalLength={0.02} bokehScale={2} height={480} />
+        <Bloom luminanceThreshold={0} luminanceSmoothing={0.9} height={300} />
+        <Noise opacity={0.02} />
+        <Vignette eskil={false} offset={0.1} darkness={1.1} />
+      </EffectComposer>
     </Canvas>
   )
 }
